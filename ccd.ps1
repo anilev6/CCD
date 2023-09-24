ef4dde53f82432ebeb4f951a139aba46c1c0af7b
ani <125677472+anilev6@users.noreply.github.com>
2023-09-24 16:47:36 +0200

    upd

--- ccd.ps1
+++ ccd.ps1
@@ -1,36 +1,3 @@
-# function Move-To-NextDir {
-#     $current_dir_name = Split-Path -Leaf (Get-Location)
-    
-#     # Get all subdirectories
-#     $dirs = Get-ChildItem -Directory
-    
-#     switch ($dirs.Count) {
-#         0 {
-#             Write-Output "No directories to navigate into."
-#         }
-#         1 {
-#             Set-Location -Path $dirs.FullName
-#         }
-#         default {
-#             # Try to find an exact match based on the current directory name
-#             $exactMatch = $dirs | Where-Object { $_.Name -eq $current_dir_name }
-            
-#             if ($exactMatch) {
-#                 Set-Location -Path $exactMatch.FullName
-#             } else {
-#                 # Try to find a similar directory
-#                 $similarMatch = $dirs | Where-Object { $_.Name -like "*$current_dir_name*" } | Select-Object -First 1
-                
-#                 if ($similarMatch) {
-#                     Set-Location -Path $similarMatch.FullName
-#                 } else {
-#                     Write-Output "Couldn't find a matching directory. Not navigating automatically."
-#                 }
-#             }
-#         }
-#     }
-# }
-
 # Move-To-NextDir
 function Move-To-NextDir {
     $current_dir_name = Split-Path -Leaf (Get-Location)
@@ -52,7 +19,7 @@ function Move-To-NextDir {
             $exactMatch = ($dirs | Where-Object { $_.Name -eq $current_dir_name } | Select-Object -First 1).FullName
 
             if ($exactMatch) {
-                return $exactMatch.FullName  # <- This one is redundant
+                return $exactMatch.FullName  
             } else {
                 # Try to find a similar directory
                 $similarMatch = ($dirs | Where-Object { $_.Name -like "*$current_dir_name*" } | Select-Object -First 1).FullName
@@ -71,5 +38,4 @@ function Move-To-NextDir {
 $path = Move-To-NextDir
 if ($path) {
     Set-Location -Path $path
-    # Write-Output "Debug: path is $path"
 }
